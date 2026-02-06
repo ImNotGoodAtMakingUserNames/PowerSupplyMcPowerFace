de4e0eaa89259caa81fafe107ffb1319087d3dce
ImBadAtMakingUsernames <146992662+ImNotGoodAtMakingUserNames@users.noreply.github.com>
2026-02-05 19:31:12 -0800

    Finally got LVGL to run on my laptop. Visual Studio FTW.

--- src/monitoring/monitoring.ino
+++ src/monitoring/monitoring.ino
@@ -1,15 +1,9 @@
 #include <Wire.h>
 
 
-#define I2C_SDA 38
-#define I2C_SCL 39
-#define CHILD_ADDR 0x12
-
-#define HL_PIN 40
-
-uint32_t msgCounter = 0;
-unsigned long lastSent = 0;
-
+#define I2C_SDA 38 // Data pin
+#define I2C_SCL 39 // Clock pin
+#define CHILD_ADDR 0x12 // Child Address(es) for rail monitoring
 
 
 void setup() {
