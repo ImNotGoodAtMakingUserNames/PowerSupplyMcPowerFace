4b855c2ec27d3eb36d33f74fe01af462a8624c29
ImBadAtMakingUsernames <146992662+ImNotGoodAtMakingUserNames@users.noreply.github.com>
2026-02-07 20:18:46 -0800

    More framework

--- src/monitoring/monitoring.ino
+++ src/monitoring/monitoring.ino
@@ -1,18 +1,41 @@
 #include <Wire.h>
 
-#define I2C_SDA 38 // Data pin
-#define I2C_SCL 39 // Clock pin
+
+// Pin assignments
+#define I2C_SDA  38   // INPUT  Data pin
+#define I2C_SCL  39   // INPUT  Clock pin
+#define PWR_GOOD 40   // INPUT  Power Good
+#define PWR_BAD  41   // OUTPUT Power Bad
+
+// I2C input address
 #define TELEM_V12_ADDR 0x40 
 #define TELEM_V3_ADDR  0x41
 #define TELEM_V5_ADDR  0x44
 
 
+// Global Variables
+uint32_t boot_time = NULL;
+uint32_t time_since_pwr_bad = NULL;
+uint32_t time_since_last_measure = NULL;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(PWR_GOOD, INPUT);
+  pinMode(PWR_BAD,  OUTPUT);
+
+
+  Wire.begin(I2C_SDA,I2C_SCL);
+  if(!lox.begin()){
+    Serial.println("Failed to detect I2C line, check connection. Suspending system...");
+    while(1);
+  }
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 
 }
