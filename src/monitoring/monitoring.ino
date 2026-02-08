83f6f92e4436d0aeaeeebf6137514056e3e5dd82
ImBadAtMakingUsernames <146992662+ImNotGoodAtMakingUserNames@users.noreply.github.com>
2026-02-07 18:35:12 -0800

    Monitoring: V telem addrs added

--- src/monitoring/monitoring.ino
+++ src/monitoring/monitoring.ino
@@ -1,9 +1,10 @@
 #include <Wire.h>
 
-
 #define I2C_SDA 38 // Data pin
 #define I2C_SCL 39 // Clock pin
-#define CHILD_ADDR 0x12 // Child Address(es) for rail monitoring
+#define TELEM_V12_ADDR 0x40 
+#define TELEM_V3_ADDR  0x41
+#define TELEM_V5_ADDR  0x44
 
 
 void setup() {
